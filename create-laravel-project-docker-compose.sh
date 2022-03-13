dcbed2dec8c11ebf9bcbb88dee9fc11659fa3516
vscalcione <vincenzo99.scalcione@gmail.com>
2022-03-13 15:44:24 +0100

    Update

--- create-laravel-project-docker-compose.sh
+++ create-laravel-project-docker-compose.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
 PROJECT_NAME=${1?Error: No PROJECT_NAME given}
-
 curl -s https://laravel.build/$PROJECT_NAME | bash
 
-if [$? -eq 0 ]; then
-    echo 'Project generated with success!';
-else
-    echo 'Project not generated';
-fi
